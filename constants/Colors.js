002a06eeded9df173cd74f65949eb4a6c6f87d5e
Adrian Orłów <aorlow@mail.com>
2019-10-19 01:31:51 +0200

    Add: Constants

---
+++ constants/Colors.js
@@ -0,0 +1,9 @@
+export default {
+  white: '#fff',
+  whiteGreenish: '#E0ECE6',
+  greyGreenish: '#9DA8A7',
+  grey: '#f5f5f5',
+  green: '#447B66',
+  greenLight: '',
+  blackGreenish: '#4C5B5D'
+};
